17f7b7a3066b53020a5519c71be80b877b74d005
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2025-06-22 23:26:27 +0530

    initial commit

---
+++ src/main/java/com/example/rscarpoint/RscarpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.rscarpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RscarpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RscarpointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rscarpoint/RscarpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rscarpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RscarpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
